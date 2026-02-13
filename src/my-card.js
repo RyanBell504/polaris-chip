51fd91d8d2de4eb19480b62e1776cc7e2c1f9947
Ryan <rrb5422@psu.edu>
2026-02-13 10:26:13 -0500

    uniform and details

--- src/my-card.js
+++ src/my-card.js
@@ -18,6 +18,7 @@ export class MyCard extends LitElement {
     this.description="DEFAULT DESCRIPTION";
     this.image="https://gopsusports.com/imgproxy/WGYkyuIKGU5T0IO-JEftbXTHodp6K-Il-oM_MFSwaiE/rs:fit:1980:0:0:0/g:ce:0:0/q:90/aHR0cHM6Ly9zdG9yYWdlLmdvb2dsZWFwaXMuY29tL2dvcHN1c3BvcnRzLXByb2QvMjAyMy8wNy8xMy9DNFMycW5DRzhxcE1JNHBYTGhHT1hacng1Qkdrd0FURDNMZnMwdG9CLndlYnA.webp";
     this.link="https://hax.psu.edu";
+    this.location="University Park, PA";
     this.fancy=false;
   }
 
@@ -29,24 +30,30 @@ export class MyCard extends LitElement {
 }
 :host([fancy]){
   display: inline-block;
-  box-shadow: 10px 5px 5px darkblue
+  box-shadow: 10px 5px 5px #0a0044;
 }
 .card{
   width: 400px;
   height: 500px;
-  background-color: var(--psu-blue, #001e62);
+  background-color: var(--psu-blue);
   border: solid 2px;
 }
 .pic {
   margin: 0px 50px;
-  border: 5px solid;
+  border: 5px white solid;
   width: 300px;
   height: 200px;
   aspect-ratio: 3/2;
 }
 .btn{
-  margin: 100px 150px;
+  margin: 50px 150px;
   padding: 10px;
+  font-size: 15px;
+  background-color: white;
+}
+.location{
+  color: white;
+  text-align: center;
 }
 .description{
   font-size: 16px;
@@ -72,6 +79,7 @@ export class MyCard extends LitElement {
       ${this.title}
     </h1>
     <img class=pic src=${this.image}>
+    <slot class="location"><p>${this.location}</p></slot>
   <details class="description" ?open="${this.fancy}" @toggle="${this.openChanged}">
   <summary>Description</summary>
   <div>
@@ -101,6 +109,7 @@ openChanged(e) {
       image: { type: String },
       description: { type: String },
       link: { type: String  },
+      location: { type: String },
       fancy: { type: Boolean, reflect: true },
     };
   }
